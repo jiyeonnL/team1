f749265084ff7e27f478433e06243ef64efa7956
sasa5680 <sasa5680@naver.com>
2021-12-30 14:45:39 +0900

    오류 수정

--- src/main/java/com/team1/controller/board/HelpController.java
+++ src/main/java/com/team1/controller/board/HelpController.java
@@ -3,7 +3,6 @@ package com.team1.controller.board;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,20 +17,15 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.team1.coverData.Cover;
 import com.team1.domain.board.HelpVO;
 import com.team1.service.board.HelpService;
-import com.team1.service.user.UserService;
 
 import lombok.Setter;
 
 @Controller
 @RequestMapping("/help")
-
 public class HelpController {
 	
 	@Setter(onMethod_ = @Autowired)
 	private HelpService service;
-	
-	@Setter(onMethod_ = @Autowired)
-	private UserService userService;
 	
 	//help 페이지 (검색 쿼리 있는 버전)
 	@GetMapping(value = "/list", params = { "location", "query" })
@@ -43,11 +37,10 @@ public class HelpController {
 		model.addAttribute("tag", "help");
 		model.addAttribute("location", location);
 		
-		Cover.setCover("help", model);
-		List<HelpVO> list = service.getList();
+		List<HelpVO> list = service.getListSearchByContent(query);
 
 		model.addAttribute("list", list);
-
+		
 	}
 	
 	//help 페이지
@@ -59,15 +52,10 @@ public class HelpController {
 		model.addAttribute("tag", "help");
 		model.addAttribute("location", location);
 		
-		List<HelpVO> listSearch = service.getListSearchByContent(query);
-//		PageInfoVO pageInfo = service.getPageInfo(page, numberPerPage, numberPerPagination);
-		  
-		System.out.println("listSearch"+listSearch);
-		model.addAttribute("listSearch", listSearch);
+		List<HelpVO> list = service.getList();
 
-		
+		model.addAttribute("list", list);
 	}
-		
 	
 	//게시물 상세 페이지, help/list/id 와 같은 형식으로 게시물의 id를 링크에서 가져온다.
 	@GetMapping(value = "/list/{id}")
@@ -86,19 +74,21 @@ public class HelpController {
 
 	
 	@GetMapping({"/get", "/modify"})
-	public void get(@RequestParam("id") Integer id, Model model, HttpSession session) {
+	public void get(@RequestParam("id") Integer id, Model model) {
 		HelpVO board = service.get(id);
 		String nickname = service.getNick(id);
-//		String nick = "EasyMoneySniper";
-//		UserVO loggedmember = userService.read(nick);
-//		session.setAttribute("loggedInMember", loggedmember);로그인이 안돼서 수동으로 만들어준 로그인 정보 객체
+		System.out.println(board);
+		System.out.println(nickname);
 		model.addAttribute("board", board);
 		model.addAttribute("nickname", nickname);
 	}
 	
 	@PostMapping("/modify")
 	public String modify(HelpVO board, RedirectAttributes rttr) {
+		System.out.println("/modify로 잘 옴.");
+		System.out.println(board);
 		if (service.modify(board)) {
+			System.out.println("수정됨");
 			rttr.addFlashAttribute("result", board.getId() + "번 게시글이 수정되었습니다.");
 		}
 		return "redirect:/help/list";
