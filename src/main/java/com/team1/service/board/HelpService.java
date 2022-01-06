d87d51d1ba0882b13ada9d364a91b639f16e2282
Haejin Yoon <hjyoomp@gmail.com>
2022-01-07 04:51:12 +0900

    야근 종료인 줄 알았는데 아니었고
    - post 댓글, 이미지 테이블 디자인 변경

--- src/main/java/com/team1/service/board/HelpService.java
+++ src/main/java/com/team1/service/board/HelpService.java
@@ -28,7 +28,6 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 @Service
 public class HelpService {
