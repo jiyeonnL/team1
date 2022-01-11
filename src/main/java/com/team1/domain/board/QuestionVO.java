1c8c07ab99056bb40ba348437616d38c6b2cb5f9
kjk732 <89769628+kjk732@users.noreply.github.com>
2022-01-11 17:58:30 +0900

    질문판 css수정 일부 색 제거, 위치조정 등 프로필 사진 첨부

--- src/main/java/com/team1/domain/board/QuestionVO.java
+++ src/main/java/com/team1/domain/board/QuestionVO.java
@@ -19,6 +19,7 @@ public class QuestionVO {
 	private LocalDateTime inserted;
 	private LocalDateTime updated;
 	private String nickname;
+	private String profileurl;
 	
 	// 새로운 글 표시 해줄 기준 (오늘 - 작성일 < 지정한 일수) 지정한 일 수는 jsp 제목 부분 c:if 에서 직접 설정
 	private Integer newMark;
